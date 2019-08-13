6b6f8f47a2f399bbb433e59c3b9ec7ee03b84086
asantope <asantope@UDI-C8ZT9R2.usersad.everis.int>
2019-08-13 14:04:12 -0300

    metodo para imprimir o nome da váriavel

--- Selenium/src/main/java/pages/PageHome.java
+++ Selenium/src/main/java/pages/PageHome.java
@@ -1,5 +1,8 @@
 package pages;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
@@ -14,7 +17,35 @@ public class PageHome extends BasePage {
 	}
 
 	MapsMenu mapsMenu;
-
+	
+	//Metodo para imprimir o nome da váriavel
+	  private void AccessFieldsUsingReflection(Object obj){
+	      Field[] fields = obj.getClass().getDeclaredFields();
+	      for(int i = 0; i < fields.length ; i++){
+	         String fieldName = fields[i].getName();
+	         Object fieldType = fields[i].getGenericType();
+	  
+	         System.out.println("Field(variable) name: " + fieldName);
+	         System.out.println("Generic Type: " + fieldType);
+	   
+	         /* Set<E>, List<E> and Map<K,V> are ParameterizedType */
+	         if(fieldType instanceof ParameterizedType){
+	           /**
+	            * This will give you Actual Type of Set<E>, List<E> and Map<K,V>.
+	            * Example:
+	            * List<String> -> String is ActualType
+	            * Set<Integer> -> Integer is ActualType
+	            * Map<String, Long> -> String, Long is ActualType
+	            */
+	            System.out.println("Actual Type:");
+	            for(Object objActualType : ((ParameterizedType) fieldType).getActualTypeArguments()){
+	                 System.out.println("-- "+objActualType);
+	            }
+	         }
+	         System.out.println("+++++++++++++++++++++++++++++++");
+	      }
+	   }
+	
 	/**
 	 * pesquisa no campo na barra superior
 	 * 
@@ -23,6 +54,8 @@ public class PageHome extends BasePage {
 	 */
 	public void pesquisaAlgo(String valor) throws Exception {
 		clickGenerico(mapsMenu.inputPesquisa2, "Campo de Pesquisar");
+		AccessFieldsUsingReflection(mapsMenu.inputPesquisa2);
+
 		sendKeysGenerico(mapsMenu.inputPesquisa2, valor, "Campo pesquisar");
 		clickGenerico(mapsMenu.btnPesquisa2, "Botão pesquisar");
 	}
